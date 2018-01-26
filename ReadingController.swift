43897af0559003078cfb634596263c76563bf760
Michal Kolbusz <mkolbusz@Michals-iMac.local>
2018-01-26 10:37:27 +0100

    adding methods

--- ReadingController.swift
+++ ReadingController.swift
@@ -10,6 +10,12 @@ import UIKit
 import CoreData
 
 
+class ReadingTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var sensorNameLabel: UILabel!
+    @IBOutlet weak var readingValueLabel: UILabel!
+}
+
 
 class ReadingController: UIViewController, UITableViewDataSource {
     
@@ -17,8 +23,11 @@ class ReadingController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var readingsTableView: UITableView!
     
     override func viewDidLoad() {
-        readingsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        generateReadings(number: 10)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadReadings()
         readingsTableView.reloadData()
     }
     
@@ -29,75 +38,32 @@ class ReadingController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let reading = readings[indexPath.row]
         
-        let cell = readingsTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = String(describing: reading.value(forKey: "value")!)
+        let cell = readingsTableView.dequeueReusableCell(withIdentifier: "ReadingCell", for: indexPath) as! ReadingTableViewCell
+        cell.readingValueLabel?.text = String(describing: reading.value(forKey: "value")!)
+        let sensor:NSManagedObject = reading.value(forKey: "sensor") as! NSManagedObject
+        
+        cell.sensorNameLabel?.text = String(describing: sensor.value(forKey: "name")!)
         return cell
     }
     
-    func generateReadings(number: Int) {
-        
-        for _ in 1...number {
-            let value = randomFloat(min: 0.0, max: 100.0)
-            let timestamp = generateRandomDate(daysBack: 365);
-            self.save(value: value, timestamp: timestamp!)
-        }
-    }
     
-    func randomFloat(min: Float, max: Float) -> Float {
-        return ((Float(arc4random()) / Float(UInt32.max)) * (max - min) + min)
-    }
-
-    func generateRandomDate(daysBack: Int)-> Date?{
-        let day = arc4random_uniform(UInt32(daysBack))+1
-        let hour = arc4random_uniform(23)
-        let minute = arc4random_uniform(59)
-        
-        let today = Date(timeIntervalSinceNow: 0)
-        let gregorian  = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
-        var offsetComponents = DateComponents()
-        offsetComponents.day = Int(day - 1)
-        offsetComponents.hour = Int(hour)
-        offsetComponents.minute = Int(minute)
-        
-        let randomDate = gregorian?.date(byAdding: offsetComponents, to: today, options: .init(rawValue: 0) )
-        return randomDate
-    }
-    
-    func save(value: Float, timestamp: Date) {
-        print("[Save] Value: \(value), Desc: \(timestamp)")
+    func loadReadings() {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
         
         let managedContext = appDelegate.persistentContainer.viewContext
-        let entity = NSEntityDescription.entity(forEntityName: "Reading", in: managedContext)!
-        let reading = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Sensor")
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Reading")
         
         do {
-            let sensors = try managedContext.fetch(fetchRequest)
-            let index = Int(arc4random_uniform(UInt32(sensors.count)))
-            if let sensor = sensors[index] as NSManagedObject? {
-                reading.setValue(value, forKey: "value")
-                reading.setValue(timestamp, forKey: "timestamp")
-                reading.setValue(sensor, forKey: "sensor")
-            }
+            readings = try managedContext.fetch(fetchRequest)
             
-            do {
-                try managedContext.save()
-                readings.append(reading)
-            } catch let error as NSError {
-                print("Could not save. \(error), \(error.userInfo)")
-            }
-
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
         }
 
-        
-        
     }
     
+    
+        
 }
--- storageComapre/HomeController.swift
+++ storageComapre/HomeController.swift
@@ -12,43 +12,181 @@ import CoreData
 class HomeController: UIViewController, UITableViewDataSource {
     
     
-    @IBOutlet weak var tableView: UITableView!
-    var sensors: [NSManagedObject] = []
+    @IBOutlet weak var resultsTableView: UITableView!
+    
+    @IBOutlet weak var numberTxt: UITextField!
+    
+    var results: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.generateSensors(number: 20)
         // Do any additional setup after loading the view, typically from a nib.
         title = "Home"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-            }
     
-   
-    
-    
+    func generateReadings(number: Int) {
+        for _ in 1...number {
+            let value = randomFloat(min: 0.0, max: 100.0)
+            let timestamp = generateRandomDate(daysBack: 365)
+            self.saveReading(value: value, timestamp: timestamp!)
+        }
+    }
     
     
     @IBAction func generateData(_ sender: UIButton) {
-
+        print("Generating")
+        let number = Int((numberTxt?.text)!)
+        self.generateReadings(number: number!)
+    }
+    
+    @IBAction func clearReadings(_ sender: UIButton) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Reading")
+        let request = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        do {
+            try managedContext.execute(request)
+        } catch let error as NSError {
+            print("Could not delete: \(error), \(error.userInfo)")
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sensors.count
+        return results.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let sensor = sensors[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = sensor.value(forKey: "name") as? String
+        let result = results[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath) as! ReadingTableViewCell
+        cell.readingValueLabel?.text = String(describing: result.value(forKey: "value")!)
+        let sensor:NSManagedObject = result.value(forKey: "sensor") as! NSManagedObject
+        
+        cell.sensorNameLabel?.text = String(describing: sensor.value(forKey: "name")!)
         return cell
     }
     
-    func save(name: String, description: String) {
-        print("Home")
+    func randomFloat(min: Float, max: Float) -> Float {
+        return ((Float(arc4random()) / Float(UInt32.max)) * (max - min) + min)
+    }
+    
+    func generateRandomDate(daysBack: Int)-> Date?{
+        let day = arc4random_uniform(UInt32(daysBack))+1
+        let hour = arc4random_uniform(23)
+        let minute = arc4random_uniform(59)
+        
+        let today = Calendar.current.date(byAdding: .year, value: -1, to: Date())!
+        let gregorian  = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
+        var offsetComponents = DateComponents()
+        offsetComponents.day = Int(day - 1)
+        offsetComponents.hour = Int(hour)
+        offsetComponents.minute = Int(minute)
+        
+        let randomDate = gregorian?.date(byAdding: offsetComponents, to: today, options: .init(rawValue: 0) )
+        return randomDate
+    }
+    
+    func saveReading(value: Float, timestamp: Date) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Reading", in: managedContext)!
+        let reading = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Sensor")
+        
+        do {
+            let sensors = try managedContext.fetch(fetchRequest)
+            let index = Int(arc4random_uniform(UInt32(sensors.count)))
+            if let sensor = sensors[index] as NSManagedObject? {
+                reading.setValue(value, forKey: "value")
+                reading.setValue(timestamp, forKey: "timestamp")
+                reading.setValue(sensor, forKey: "sensor")
+            }
+            
+            do {
+                try managedContext.save()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+    @IBAction func findLargestAndSmallestReading(_ sender: UIButton) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Reading")
+        
+        let maxP = NSPredicate(format: "value==max(value)")
+        let minP = NSPredicate(format: "value==min(value)")
+        fetchRequest.predicate = NSCompoundPredicate(orPredicateWithSubpredicates: [maxP, minP])
+        do {
+            results = try managedContext.fetch(fetchRequest)
+            resultsTableView.reloadData()
+        }catch let error as NSError {
+            print("Error: \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+    @IBAction func averageOfAllReadings(_ sender: UIButton) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSDictionary>(entityName: "Reading")
+        fetchRequest.resultType = .dictionaryResultType
+        
+        let ex = NSExpression(forFunction: "average:", arguments: [NSExpression(forKeyPath: "value")])
+        let exDesc = NSExpressionDescription()
+        exDesc.name = "avgValue"
+        exDesc.expression = ex
+        exDesc.expressionResultType = .floatAttributeType
+        
+        fetchRequest.propertiesToFetch = [exDesc]
+        
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            results.removeAll()
+            let o:NSManagedObject = NSManagedObject
+            o.setValue(result.first!.value(forKey: "avgValue"), forKey: "value")
+            results.append(o)
+        }catch let error as NSError {
+            print("Error: \(error), \(error.userInfo)")
+        }
+
+    }
+    
+    // SENSORS
+    func generateSensors(number: Int) {
+        if(self.checkIfSensorsGenerated()){
+            return
+        }
+        
+        for n in 1...number {
+            let name = "S" + (n > 9 ? String(n) : "0"+String(n))
+            let description = "Sensor number " + String(n)
+            self.saveSensor(name: name, description: description)
+        }
+    }
+    
+    func saveSensor(name: String, description: String) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
@@ -58,15 +196,32 @@ class HomeController: UIViewController, UITableViewDataSource {
         let sensor = NSManagedObject(entity: entity, insertInto: managedContext)
         
         sensor.setValue(name, forKey: "name")
-        sensor.setValue(description, forKey: "description")
+        sensor.setValue(description, forKey: "desc")
+        
         
         do {
             try managedContext.save()
-            sensors.append(sensor)
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
         }
-     }
+    }
+
+    func checkIfSensorsGenerated() -> Bool {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return false
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Sensor")
+        
+        do {
+            let number = try managedContext.count(for: fetchRequest)
+            return number  > 0 ? true : false
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        return false
+    }
     
 }
 
